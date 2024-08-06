c0f47be24f561d304ddb96f3816a9b4fea073097
Elena <olena168@gmail.com>
2024-08-06 17:59:07 -0400

    updated CV

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,11 @@ export const Header = () => {
         <div className="header__text">
           <p>with passion for learning and creating.</p>
         </div>
-        <a href="./Olena_Boichenko_CV.pdf"   download className="btn">
+        <a
+          href="./Olena_Boichenko_CV.pdf"
+          download="Olena_Boichenko_CV.pdf"
+          className="btn"
+        >
           Download CV
         </a>
       </div>
